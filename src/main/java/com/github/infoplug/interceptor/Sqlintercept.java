1367a7b800b425085951207e67319b034f8acef2
wylnoword <simondrcula@live.com>
2019-12-04 16:54:54 +0800

    first

---
+++ src/main/java/com/github/infoplug/interceptor/Sqlintercept.java
@@ -0,0 +1,21 @@
+package com.github.infoplug.interceptor;
+
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Invocation;
+
+import java.util.Properties;
+
+public class Sqlintercept implements Interceptor {
+
+    public Object intercept(Invocation invocation) throws Throwable {
+        return null;
+    }
+
+    public Object plugin(Object o) {
+        return null;
+    }
+
+    public void setProperties(Properties properties) {
+
+    }
+}
