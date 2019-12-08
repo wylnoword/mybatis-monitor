8cdba1cf93a5933ee2cc6932dcf88e59d4c7c859
simon <simondrcula@live.com>
2019-12-08 17:08:59 +0800

    init query

---
+++ src/main/java/com/github/mybatis/monitor/dao/TUserMapper.java
@@ -0,0 +1,43 @@
+package com.github.mybatis.monitor.dao;
+
+import java.util.List;
+
+import com.github.mybatis.monitor.entity.TUser;
+import org.apache.ibatis.annotations.Param;
+
+
+public interface TUserMapper {
+	
+    int deleteByPrimaryKey(Integer id);
+
+    int insert1(TUser record);
+    
+    int insert2(TUser record);
+
+    int insertSelective(TUser record);
+
+    TUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(TUser record);
+
+    int updateByPrimaryKey(TUser record);
+
+    List<TUser> selectUserHealthReport();
+
+    List<TUser> selectByEmailAndSex2(@Param("email")String email,@Param("sex")Byte sex);
+
+    List<TUser> selectIfandWhereOper(@Param("email")String email,@Param("sex")Byte sex);
+    
+    List<TUser> selectChooseOper(@Param("email")String email,@Param("sex")Byte sex);
+    
+    int updateIfOper(TUser record);
+    
+    int updateIfAndSetOper(TUser record);
+    
+    int insertIfOper(TUser record);
+    
+    List<TUser> selectForeach4In(String[] names);
+    
+    int insertForeach4Batch(List<TUser> users);
+    
+}
---
+++ src/main/test/test/TUserMapperTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import com.github.mybatis.monitor.dao.TUserMapper;
+import com.github.mybatis.monitor.entity.TUser;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.Assert.*;
+
+public class TUserMapperTest {
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Before
+    public void init() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        // 1.读取mybatis配置文件创SqlSessionFactory
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        inputStream.close();
+    }
+    @Test
+    public void selectByPrimaryKey() {
+        // 2.获取sqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 3.获取对应mapper
+        TUserMapper mapper = sqlSession.getMapper(TUserMapper.class);
+        // 4.执行查询语句并返回结果
+        TUser user = mapper.selectByPrimaryKey(1);
+        System.out.println(user.toString());
+    }
+}
