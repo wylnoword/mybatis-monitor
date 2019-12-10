e8aced32baaf792fa482e281b3977ea68b165675
wylnoword <simondrcula@live.com>
2019-12-10 09:57:12 +0800

    pre handler init test2

--- src/main/java/com/github/mybatis/monitor/handler/PreSqlHandler.java
+++ src/main/java/com/github/mybatis/monitor/handler/PreSqlHandler.java
@@ -1,12 +1,19 @@
 package com.github.mybatis.monitor.handler;
 
+import org.apache.ibatis.logging.jdbc.PreparedStatementLogger;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+
 import java.sql.Statement;
 
 public class PreSqlHandler implements PreHandler {
 
     public String process(Statement statement) {
-        String sql = statement.toString();
-        if(sql.contains("update")&&!sql.contains("where")){
+        MetaObject metaObjectStat = SystemMetaObject.forObject(statement);
+        PreparedStatementLogger statementLogger = (PreparedStatementLogger)metaObjectStat.getValue("h");
+        Statement statement1 = statementLogger.getPreparedStatement();
+        String sql = statement1.toString();
+        if(sql.contains("select")&&sql.contains("where")){
             System.err.println("危险SQL");
             System.err.println(sql);
         }
